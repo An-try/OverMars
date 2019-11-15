0b7752fdf27c60dae52cdcf450d075dfcf40bb9a
An_try <andrey.anderey@gmail.com>
2019-11-15 13:10:04 +0200

    ship tiles creation

--- Assets/Scripts/MonoBehaviours/Ship.cs
+++ Assets/Scripts/MonoBehaviours/Ship.cs
@@ -9,6 +9,11 @@ namespace OverMars
 
         //private List<EquiupmentItem> equiupmentItems;
 
+        private void Start()
+        {
+            InitialiseShip();
+        }
+
         private void InitialiseShip()
         {
             RebuildTiles();
@@ -23,14 +28,27 @@ namespace OverMars
             Vector2Int size = _shipItem.Size;
             string cleanTilesCode = _shipItem.CleanTilesCode;
 
-            Vector2Int starterPoint = new Vector2Int((int)_tilesContainer.localPosition.x + size.x / 2, (int)_tilesContainer.localPosition.y - size.y / 2);
+            Vector2Int starterPoint = new Vector2Int((int)_tilesContainer.localPosition.x - size.x / 2, (int)_tilesContainer.localPosition.y + size.y / 2);
+
+            int tileIndex = 0;
 
             for (int i = 0; i < size.x; i++)
             {
                 for (int j = 0; j < size.y; j++)
                 {
-                    Vector3 newTilePosition = new Vector3(starterPoint.x + j, starterPoint.y + i, 0);
-                    Instantiate(_shipItem.TilePrefab, newTilePosition, Quaternion.identity, _tilesContainer);
+                    Vector3 newTilePosition = new Vector3(starterPoint.x + j, starterPoint.y - i, 0);
+                    ShipTileBattle shipTileBattle = Instantiate(_shipItem.TilePrefab, newTilePosition, Quaternion.identity, _tilesContainer).GetComponent<ShipTileBattle>();
+
+                    if (_shipItem.CleanTilesCode[tileIndex] == '0')
+                    {
+                        shipTileBattle.DeactivateTile();
+                    }
+                    else
+                    {
+                        shipTileBattle.ActivateTile();
+                    }
+
+                    tileIndex++;
                 }
             }
         }
