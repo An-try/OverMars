e22d82e6178bcd0e76a0bc60da8902386ce15e75
An_try <andrey.anderey@gmail.com>
2019-12-02 15:45:41 +0200

    feat (UI) : add scale feature to the equipment content, add background, edit equipment items

--- Assets/Scripts/MonoBehaviours/Containers/ItemsContainer.cs
+++ Assets/Scripts/MonoBehaviours/Containers/ItemsContainer.cs
@@ -10,39 +10,27 @@ namespace OverMars
         [Header("Ships")]
         [SerializeField] private List<ShipItem> _shipItems;
 
+        [Header("Weapon")]
+        [SerializeField] private List<WeaponEquipmentItem> _weaponEquipmentItems;
+
+        [Header("Defence")]
+        [SerializeField] private List<DefenceEquipmentItem> _defenceEquipmentItems;
+
+        [Header("Utility")]
+        [SerializeField] private List<UtilityEquipmentItem> _utilityEquipmentItems;
+
 #pragma warning restore 0649
 
-        //[Header("Weapons")]
-        //[SerializeField] private List<PenetrationWeapon> _penetrationWeapons;
-        //[SerializeField] private List<RocketWeapon> _rocketWeapons;
-        //[SerializeField] private List<LaserWeapon> _laserWeapons;
-
-        //[Header("Equipment")]
-        //[SerializeField] private List<ArmorEquipment> _armorEquipments;
-        //[SerializeField] private List<ShieldEquipment> _shieldEquipments;
-        //[SerializeField] private List<ReactorEquipment> _reactorEquipments;
-        //[SerializeField] private List<EngineEquipment> _engineEquipments;
-
-        public List<ShipItem> ShipItems => _shipItems;
-        //public List<PenetrationWeapon> PenetrationWeapons => _penetrationWeapons;
-        //public List<RocketWeapon> RocketWeapons => _rocketWeapons;
-        //public List<LaserWeapon> LaserWeapons => _laserWeapons;
-        //public List<ArmorEquipment> ArmorEquipments => _armorEquipments;
-        //public List<ShieldEquipment> ShieldEquipments => _shieldEquipments;
-        //public List<ReactorEquipment> ReactorEquipments => _reactorEquipments;
-        //public List<EngineEquipment> EngineEquipments => _engineEquipments;
         public List<EquipmentItem> AllEquipmentItems
         {
             get
             {
                 List<EquipmentItem> equipmentItems = new List<EquipmentItem>();
-                //equipmentItems.AddRange(_penetrationWeapons);
-                //equipmentItems.AddRange(_rocketWeapons);
-                //equipmentItems.AddRange(_laserWeapons);
-                //equipmentItems.AddRange(_armorEquipments);
-                //equipmentItems.AddRange(_shieldEquipments);
-                //equipmentItems.AddRange(_reactorEquipments);
-                //equipmentItems.AddRange(_engineEquipments);
+
+                equipmentItems.AddRange(_weaponEquipmentItems);
+                equipmentItems.AddRange(_defenceEquipmentItems);
+                equipmentItems.AddRange(_utilityEquipmentItems);
+
                 return equipmentItems;
             }
         }
--- Assets/Scripts/MonoBehaviours/Controllers/Panels/EquipmentPanelController.cs
+++ Assets/Scripts/MonoBehaviours/Controllers/Panels/EquipmentPanelController.cs
@@ -8,9 +8,15 @@ namespace OverMars
     {
 #pragma warning disable 0649
 
+        [SerializeField] private ShipItem _shipItem;
         [SerializeField] private Transform _shipTilesContainerUI;
         [SerializeField] private Image _shipImageComponent;
         [SerializeField] private GameObject _shipTileUIPrefab;
+        [SerializeField] private RectTransform _equipmentContentRect;
+
+        [Header("References for scaling")]
+        [SerializeField] private ScrollRect _equipmentScrollRect;
+        [SerializeField] private Transform[] _objectsToScale;
 
 #pragma warning restore 0649
 
@@ -21,9 +27,36 @@ namespace OverMars
         private static Color _itemSuitableColor = Color.green;
         private static Color _itemUnsuitableColor = Color.red;
 
+        #region Fields for scaling
+
+        private int _currentTouchesCount = 0;
+
+        private float _zoomSensitivity = 2f;
+        private float _previousTouchesDelta = 0f;
+        private float _currentTouchesDelta = 0f;
+
+        private bool _alreadyPressed = false;
+
+        private const float MOUSE_SCROLL_MULTIPLIER = 2;
+        private const float MIN_SCALE = 0.2f;
+        private const float MAX_SCALE = 3f;
+
+        #endregion
+
         private void Start()
         {
             RebuildTiles();
+        }
+
+        private void Update()
+        {
+#if UNITY_EDITOR
+            ScaleObjectsWithMouse();
+#else
+            ScaleObjectsWithFingers();
+#endif
+
+            ClampEquipmentContentPosition();
         }
 
         public static void SetSlotsUnderItemAreNotEmpty()
@@ -47,13 +80,12 @@ namespace OverMars
             DestroyTiles();
             _shipTilesContainerUI.localPosition = Vector3.zero;
 
-            ShipItem shipItem = PlayerController.Instance.Ship.ShipItem;
-            _shipImageComponent.sprite = shipItem.Sprite;
-            _shipImageComponent.rectTransform.sizeDelta = new Vector2(shipItem.Sprite.rect.size.x, shipItem.Sprite.rect.size.y);
+            _shipImageComponent.sprite = _shipItem.Sprite;
+            _shipImageComponent.rectTransform.sizeDelta = new Vector2(_shipItem.Sprite.rect.size.x, _shipItem.Sprite.rect.size.y);
 
-            string cleanTilesCode = shipItem.CleanTilesCode;
-            int shipWidth = shipItem.Width;
-            int shipHeight = shipItem.Height;
+            string cleanTilesCode = _shipItem.CleanTilesCode;
+            int shipWidth = _shipItem.Width;
+            int shipHeight = _shipItem.Height;
 
             EquipmentTilesGrid = new EquipmentSlotUI[shipHeight, shipWidth];
             Vector2Int imageSize = new Vector2Int((int)_shipTileUIPrefab.GetComponent<RectTransform>().sizeDelta.x, (int)_shipTileUIPrefab.GetComponent<RectTransform>().sizeDelta.y);
@@ -110,6 +142,7 @@ namespace OverMars
 
         #endregion
 
+
         #region Check drag_and_drop_item for suitability
 
         public static void CheckDragAndDropItemForSuitability(List<Vector2Int> equipmentSlotsUnderDragAndDropObjectArrayIndexes, int itemWidth, int itemHeight)
@@ -155,6 +188,119 @@ namespace OverMars
                     EquipmentTilesGrid[arrayIndex.x, arrayIndex.y].MarkAsUnderItem(newColor);
                 }
             }
+        }
+
+        #endregion
+
+
+        #region Scaling equipment content
+
+        private void ScaleObjectsWithMouse()
+        {
+            float mouseScroll = Input.mouseScrollDelta.y * _zoomSensitivity * MOUSE_SCROLL_MULTIPLIER * Time.deltaTime;
+            Vector3 mouseWorldPosition =
+                Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 100));
+
+            if (MouseInQuarter(mouseWorldPosition) && mouseScroll != 0)
+            {
+                ZoomImage(mouseScroll);
+            }
+        }
+
+        private void ScaleObjectsWithFingers()
+        {
+            if (Input.touchCount >= 2 && AllFingersAreInFourthQuarter())
+            {
+                if (_currentTouchesCount != Input.touchCount)
+                {
+                    _previousTouchesDelta = GetDeltaMagnitudeDifference();
+                    _currentTouchesCount = Input.touchCount;
+                }
+
+                if (!_alreadyPressed)
+                {
+                    _previousTouchesDelta = GetDeltaMagnitudeDifference();
+                    _alreadyPressed = true;
+                }
+
+                _equipmentScrollRect.enabled = false;
+
+                _currentTouchesDelta = GetDeltaMagnitudeDifference();
+                float newScale = ((_currentTouchesDelta / _previousTouchesDelta) - 1) * _zoomSensitivity;
+                _previousTouchesDelta = _currentTouchesDelta;
+
+                ZoomImage(newScale);
+            }
+            else if (Input.touchCount == 1)
+            {
+                _alreadyPressed = false;
+                _equipmentScrollRect.enabled = true;
+            }
+            else
+            {
+                _alreadyPressed = false;
+            }
+        }
+
+        private void ZoomImage(float zoomingValue)
+        {
+            foreach (Transform objectToScale in _objectsToScale)
+            {
+                objectToScale.localScale += new Vector3(zoomingValue, zoomingValue, zoomingValue);
+
+                objectToScale.localScale = new Vector3(Mathf.Clamp(objectToScale.localScale.x, MIN_SCALE, MAX_SCALE),
+                                                       Mathf.Clamp(objectToScale.localScale.y, MIN_SCALE, MAX_SCALE),
+                                                       Mathf.Clamp(objectToScale.localScale.z, MIN_SCALE, MAX_SCALE));
+            }
+        }
+
+        private bool AllFingersAreInFourthQuarter()
+        {
+            //foreach (Touch touch in Input.touches)
+            //{
+            //    Vector3 touchWorldCoordinates = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 100));
+
+            //    if (touchWorldCoordinates.x < _resizeButton.transform.position.x || touchWorldCoordinates.y > _resizeButton.transform.position.y)
+            //    {
+            //        return false;
+            //    }
+            //    return true;
+            //}
+            return true;
+        }
+
+        private bool MouseInQuarter(Vector3 mouseWorldPosition)
+        {
+            //if (mouseWorldPosition.x < _resizeButton.transform.position.x ||
+            //    mouseWorldPosition.y > _resizeButton.transform.position.y)
+            //{
+            //    return false;
+            //}
+            return true;
+        }
+
+        private float GetDeltaMagnitudeDifference()
+        {
+            float result = 0;
+            for (int i = 0; i < Input.touches.Length - 1; ++i)
+            {
+                for (int j = i + 1; j < Input.touches.Length; ++j)
+                {
+                    result += Vector2.SqrMagnitude(Input.touches[i].position - Input.touches[j].position);
+                }
+            }
+            return result / (Input.touches.Length * Input.touches.Length);
+        }
+
+        #endregion
+
+
+        #region Clamping equipment content position
+
+        private void ClampEquipmentContentPosition()
+        {
+            _equipmentContentRect.anchoredPosition = new Vector2(Mathf.Clamp(_equipmentContentRect.anchoredPosition.x, -Screen.width / 2, Screen.width / 2),
+                                                                 Mathf.Clamp(_equipmentContentRect.anchoredPosition.y, -Screen.height / 2, Screen.height / 2));
         }
 
         #endregion
